c79c60748f33b49e1627a41b55d403d13b08d09a
Chandhana sanoj <chandhanasanoj03@gmail.com>
2025-07-21 12:41:43 +0530

    Update treasure-island.py

--- Day3/treasure-island.py
+++ Day3/treasure-island.py
@@ -1,4 +1,4 @@
-print(''' *******************************************************************************
+print(''' **********************************************************************
           |                   |                  |                     |
  _________|________________.=""_;=.______________|_____________________|_______
 |                   |  ,-"_,=""     `"=.|                  |
@@ -19,3 +19,23 @@ ____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
 /______/______/______/______/______/______/______/______/______/______/[TomekK]
 *******************************************************************************
 ''')
+print("welcome to treasure island")
+print("Your mission is to find the find the treasure.")
+choise1 = input('You are at a crossover, where do u want to  go first? Type "left" or "right".').lower()
+if choise1 == "left":
+    choise2 = input('You have come to a lake. there is an island in the middle of the lake. Type "wait" to wait for a boat. Type "swim" to swim across.').lower()
+    if choise2 == "wait":
+        choise3 = input('You arrived at the island unharmed. There is a house with 3 doors: one red, one yellow, and one blue. Which colour would you choose?').lower()
+        if choise3 == "red":
+            print("It is a room full of fire. Game over.")
+        elif choise3 == "yellow":
+            print("You found the treasure. You win.")
+        elif choise3 == "blue":
+            print("You enter a room full of poison. Game over.")
+        else:
+            print("You choose a door that does not exist. Game over.")
+    else:
+        print("You are attacked by an alligator. Game over.")
+else:
+    print("You fell into a hole. Game over.")
+
